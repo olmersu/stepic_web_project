4abee2f4eabe06094a1c4ed968ee1c2837fabceb
olmersu <olmersu@gmail.com>
2016-11-25 00:01:04 +0800

    third

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/gunicorn.conf
+gunicorn -c /home/box/web/etc/hello.conf -b 0.0.0.0:8080 hello:hello_app &
