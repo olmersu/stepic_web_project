cbd69433531b01f3217d5be2f2de08cd64502d6f
olmersu <olmersu@gmail.com>
2016-11-24 18:21:31 +0800

    second

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿/bin/ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
