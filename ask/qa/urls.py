b884fb669a7104048eb1b508621513ef34db7b9f
olmersu <olmersu@gmail.com>
2016-11-26 05:28:13 +0000

    forth

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^login/', views.test, name='login'),
+    url(r'^signup/', views.test, name='signup'),
+    url(r'^question/(.+)/', views.test, name='detail'),
+    url(r'^ask/', views.test, name='ask'),
+    url(r'^popular/', views.test, name='popular'),
+    url(r'^new/', views.test, name='new'),
+]
